f56fecf77397893c436acc68adac06472763ebd0
NunezManriqueCuauhtemocEmmanuel <cnunezm1900@alumno.ipn.mx>
2026-05-24 12:15:50 -0600

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
       <main>
         <div className="main_titulo">
           <h1>Datara</h1>
         </div>
       </main>
+      <section className="contenedor__contenido">
+
+        <div className="contenedor__instrucciones">
+          <h3>En este proyecto permite simular, analizar y visualizar diferentes distribuciones de probabilidad mediante la generación de datos aleatorios.</h3>
+          <h4>¿Qué puedes hacer aqui?</h4>
+          <p>El propósito es relacionar los conceptos sobre distribuciones con una implementación computacional, de esta manera podrás llevar acabo:</p>
+
+          <div className="contenedor__herramientas">
+            <div className="herramienta">
+              <div className="contenedor_herramienta">
+              </div>
+              <h5>observar sus cambios gráficos</h5>
+            </div>
+            <div className="herramienta">
+              <div className="contenedor_herramienta">
+                
+              </div>
+            </div>
+            <div className="herramienta">
+              <div className="contenedor_herramienta">
+                
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </>
   )
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/navbar.css'
+
+const navbar = () => {
+    return (
+        <nav>
+            <div className="contenedor__nav">
+                
+            </div>
+        </nav>
+    )
+}
+
+export default navbar
